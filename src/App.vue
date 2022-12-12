89c27b10734523566f555128f90591c009eff2ae
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-12 14:34:04 +0300

    reset css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
+    <header>
 
+
+    </header>
   </div>
 </template>
 
 <style lang="scss">
+body{
+  width: 100vh;
+  background: #EADDFF;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
 </style>

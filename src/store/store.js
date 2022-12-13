fb1b9b924164011991323ba4d5acd284f9a7e74c
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-13 17:05:41 +0300

    move api

--- src/store/store.js
+++ src/store/store.js
@@ -1,36 +1,32 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    search:''
+    search: "",
   },
-  getters: {
-    
-  },
+  getters: {},
   mutations: {
+ 
+  },
+  actions: {
     getAPI() {
-      axios('https://www.googleapis.com/books/v1/volumes?q=english&key=AIzaSyAXj8Lh7xnbblNxlI1m-pIuRq-3mayhb6s')
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=english&key=AIzaSyAXj8Lh7xnbblNxlI1m-pIuRq-3mayhb6s"
+        )
         .then((response) => {
-          for (let i = 0; i < response.data.items.length; i++) {
-            let item = response.data.items[i];
+          response.data.items.map((item) => {
             console.log(item.volumeInfo.title);
-          }
+          });
         })
         .catch((error) => {
           console.log(error);
         });
     },
   },
-  actions: {
-    getAPI({commit}){
-      commit('getAPI')
-    }
-   
-  },
-  modules: {
-  }
-})
+  modules: {},
+});

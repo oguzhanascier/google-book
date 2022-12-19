9a46788016b8e19e6f1384c2f86c57af68fb84b9
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-19 09:37:08 +0300

    clear

--- src/store/store.js
+++ src/store/store.js
@@ -22,15 +22,9 @@ export default new Vuex.Store({
           "https://www.googleapis.com/books/v1/volumes?q=english&key=AIzaSyAXj8Lh7xnbblNxlI1m-pIuRq-3mayhb6s"
         )
         .then((response) => {
-            // let book = response;
-            // commit("createBooks", book);
-            // console.log(response.volumeInfo.subtitle);
-            for (let i = 0; i < response.data.items.length; i++) {
-              let item = response.data.items[i];
-              console.log(item.volumeInfo.title);
+      
               let book = response;
               commit("createBooks", book.data.items);
-            }
         })
         .catch((error) => {
           console.log(error);
